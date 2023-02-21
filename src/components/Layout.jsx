57a154988e321cf9297d7e646bd2bcf55f984558
ericdiviney <etdiviney14@yahoo.com>
2023-02-21 16:20:13 -0600

    Radix scroll area works great :) (#5)
    Co-authored-by: Eric Diviney <ericdiviney@DALML6185.attlocal.net>

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,3 +1,4 @@
+import * as ScrollArea from '@radix-ui/react-scroll-area';
 import Link from 'next/link'
 import { motion } from 'framer-motion'
 
@@ -16,14 +17,21 @@ export function Layout({ children, sections = [] }) {
           layoutScroll
           className="contents lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex"
         >
-          <div className="contents lg:pointer-events-auto lg:block lg:w-72 lg:overflow-y-auto lg:border-r lg:border-zinc-900/10 lg:px-6 lg:pt-4 lg:pb-8 lg:dark:border-white/10 xl:w-80">
-            <div className="hidden lg:flex">
-              <Link href="/" aria-label="Home">
-                <Logo className="h-6" />
-              </Link>
-            </div>
-            <Header />
-            <Navigation className="hidden lg:mt-10 lg:block" />
+          <div className="contents lg:pointer-events-auto lg:block lg:w-72 lg:border-r lg:border-zinc-900/10 lg:px-6 lg:pt-4 lg:pb-8 lg:dark:border-white/10 xl:w-80">
+            <ScrollArea.Root className="w-full h-full overflow-hidden">
+              <ScrollArea.Viewport className="w-full h-full rounded">
+                <div className="hidden lg:flex">
+                  <Link href="/" aria-label="Home">
+                    <Logo className="h-6" />
+                  </Link>
+                </div>
+                <Header />
+                <Navigation className="hidden lg:mt-10 lg:block" />
+              </ScrollArea.Viewport>
+              <ScrollArea.Scrollbar orientation="vertical">
+                <ScrollArea.Thumb />
+              </ScrollArea.Scrollbar>
+            </ScrollArea.Root>
           </div>
         </motion.header>
         <div className="relative px-4 pt-14 sm:px-6 lg:px-8">
