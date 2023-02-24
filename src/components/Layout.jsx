32b0da48b3ba5376c7196dbaf4814354d323075c
Eric Diviney <ericdiviney@unknown16ebb606893e.attlocal.net>
2023-02-24 17:20:14 -0600

    Try to fix scroll issue on mobile

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,8 +8,14 @@ import { Logo } from '@/components/Logo'
 import { Navigation } from '@/components/Navigation'
 import { Prose } from '@/components/Prose'
 import { SectionProvider } from '@/components/SectionProvider'
+import { useMobileDetect } from '@/lib/useMobileDetect'
 
 export function Layout({ children, sections = [] }) {
+  const currentDevice = useMobileDetect()
+
+  const isMobile = currentDevice.isMobile()
+  const isDesktop = currentDevice.isDesktop()
+  
   return (
     <SectionProvider sections={sections}>
       <div className="lg:ml-72 xl:ml-80">
@@ -17,8 +23,8 @@ export function Layout({ children, sections = [] }) {
           layoutScroll
           className="contents lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex"
         >
-          <div className="contents lg:pointer-events-auto lg:block lg:w-72 lg:border-r lg:border-zinc-900/10 lg:px-6 lg:pt-4 lg:pb-8 lg:dark:border-white/10 xl:w-80">
-            <ScrollArea.Root className="w-full h-full overflow-hidden">
+          <div className="contents lg:pointer-events-auto lg:block lg:w-72 lg:overflow-y-auto lg:border-r lg:border-zinc-900/10 lg:px-6 lg:pt-4 lg:pb-8 lg:dark:border-white/10 xl:w-80">
+            {isDesktop ? (<ScrollArea.Root className="w-full h-full overflow-hidden">
               <ScrollArea.Viewport className="w-full h-full rounded">
                 <div className="hidden lg:flex">
                   <Link href="/" aria-label="Home">
@@ -34,7 +40,17 @@ export function Layout({ children, sections = [] }) {
               >
                 <ScrollArea.Thumb className="flex-1 bg-zinc-800 rounded-[10px] relative before:content-[''] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:w-full before:h-full before:min-w-[44px] before:min-h-[44px]" />
               </ScrollArea.Scrollbar>
-            </ScrollArea.Root>
+            </ScrollArea.Root>) : (
+              <>
+                <div className="hidden lg:flex">
+                <Link href="/" aria-label="Home">
+                  <Logo className="h-6" />
+                </Link>
+              </div>
+              <Header />
+              <Navigation className="hidden lg:mt-10 lg:block" />
+              </>
+            )}
           </div>
         </motion.header>
         <div className="relative px-4 pt-14 sm:px-6 lg:px-8">
