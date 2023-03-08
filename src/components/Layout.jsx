d2a90ce4d73b203b7b558c5d02063a3ad6b0d5e0
Eric Diviney <ericdiviney@DALML6185.attlocal.net>
2023-03-08 17:41:39 -0600

    Fix scrollbar issue in Next and push writing updates to pages

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,3 @@
-import * as ScrollArea from '@radix-ui/react-scroll-area';
 import Link from 'next/link'
 import { motion } from 'framer-motion'
 
@@ -8,13 +7,20 @@ import { Logo } from '@/components/Logo'
 import { Navigation } from '@/components/Navigation'
 import { Prose } from '@/components/Prose'
 import { SectionProvider } from '@/components/SectionProvider'
-import { useMobileDetect } from '@/lib/useMobileDetect'
 
 export function Layout({ children, sections = [] }) {
-  const currentDevice = useMobileDetect()
 
-  const isMobile = currentDevice.isMobile()
-  const isDesktop = currentDevice.isDesktop()
+  const defaultSidebar = (
+    <>
+      <div className="hidden lg:flex">
+        <Link href="/" aria-label="Home">
+          <Logo className="h-6" />
+        </Link>
+      </div>
+      <Header />
+      <Navigation className="hidden lg:mt-10 lg:block" />
+    </>
+  )
   
   return (
     <SectionProvider sections={sections}>
@@ -24,33 +30,7 @@ export function Layout({ children, sections = [] }) {
           className="contents lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex"
         >
           <div className="contents lg:pointer-events-auto lg:block lg:w-72 lg:overflow-y-auto lg:border-r lg:border-zinc-900/10 lg:px-6 lg:pt-4 lg:pb-8 lg:dark:border-white/10 xl:w-80">
-            {isDesktop ? (<ScrollArea.Root className="w-full h-full overflow-hidden">
-              <ScrollArea.Viewport className="w-full h-full rounded">
-                <div className="hidden lg:flex">
-                  <Link href="/" aria-label="Home">
-                    <Logo className="h-6" />
-                  </Link>
-                </div>
-                <Header />
-                <Navigation className="hidden lg:mt-10 lg:block" />
-              </ScrollArea.Viewport>
-              <ScrollArea.Scrollbar
-                className="flex select-none touch-none p-0.5 bg-zinc-900 transition-colors duration-[160ms] ease-out data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:h-1"
-                orientation="vertical"
-              >
-                <ScrollArea.Thumb className="flex-1 bg-zinc-800 rounded-[10px] relative before:content-[''] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:w-full before:h-full before:min-w-[44px] before:min-h-[44px]" />
-              </ScrollArea.Scrollbar>
-            </ScrollArea.Root>) : (
-              <>
-                <div className="hidden lg:flex">
-                <Link href="/" aria-label="Home">
-                  <Logo className="h-6" />
-                </Link>
-              </div>
-              <Header />
-              <Navigation className="hidden lg:mt-10 lg:block" />
-              </>
-            )}
+            {defaultSidebar}
           </div>
         </motion.header>
         <div className="relative px-4 pt-14 sm:px-6 lg:px-8">
