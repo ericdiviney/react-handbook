27982e8d3edf6cf53115298fde1f2d35810d716f
Eric Diviney <ericdiviney@DALML6185.local>
2023-03-24 13:23:09 -0500

    Formatting.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -48,7 +48,6 @@ export default function App({ Component, pageProps }) {
           href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest"></link>
-        {/* <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2290%22>🗂️</text></svg>" /> */}
       </Head>
       <MDXProvider components={mdxComponents}>
         <Layout {...pageProps}>
