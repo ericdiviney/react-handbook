21abc782f2b23f9f1edf16cd2907e2eb1f6d01b2
hrom1 <hrom012@gmail.com>
2023-03-25 21:43:59 +0200

    fix: hover Card appears below sidebar links

--- src/components/Contributors.jsx
+++ src/components/Contributors.jsx
@@ -82,7 +82,7 @@ function MemberCard({ person, large = false }) {
       </HoverCard.Trigger>
       <HoverCard.Portal>
         <HoverCard.Content
-          className="data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade w-[300px] rounded-md bg-white p-5 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] data-[state=open]:transition-all"
+          className="z-40 data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade w-[300px] rounded-md bg-white p-5 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] data-[state=open]:transition-all"
           sideOffset={5}
         >
           <div className="flex flex-col gap-[7px]">
