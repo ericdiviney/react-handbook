6522bdc8743f6795b65a4f6c42ef82b23182c8dd
Eric Diviney <ericdiviney@DALML6185.local>
2023-05-24 13:10:37 -0500

    Final updates

--- src/components/StateTldr.jsx
+++ src/components/StateTldr.jsx
@@ -49,8 +49,8 @@ const AccordionTrigger = React.forwardRef(
           aria-hidden
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M19.5 8.25l-7.5 7.5-7.5-7.5"
           />
         </svg>
